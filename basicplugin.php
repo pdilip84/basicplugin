30d1dc2033e3add69f132c6c26be5b69c6ed2f2d
pdilip84 <parmardilip2013@gmail.com>
2024-04-06 12:09:33 +0530

    create a shortcode

--- basicplugin.php
+++ basicplugin.php
@@ -15,13 +15,17 @@
  Step - 1: Create plugin file, apply security rules like blank index file & ABSPATH check.
 
  Step - 2: Run activation, deactivation & uninstall hook
+
+ Step - 3: Create a simple short-code to see if it works or not in frontend.
 */
 // For security purpose we creted a blank index.php file and we checked if ABSPATH is defined or not. It is defined if your plugin directory is accessed by default WordPress structure. If someone tied to access plugin file directlry through URL, It is not permitted.
 
+/* step 1*/
 if (!defined('ABSPATH')) {
     die('No access');
 }
 
+/* step 2*/
 function basic_activation_fun()
 {
 }
@@ -31,3 +35,11 @@ function basic_deactivation_fun()
 {
 }
 register_deactivation_hook(__FILE__, 'basic_deactivation_fun');
+
+/* step 3 */
+
+function basic_plugin_shortcode()
+{
+    return "<H1>This is a basic plugin</H1>";
+}
+add_shortcode('basic-plugin', 'basic_plugin_shortcode');
