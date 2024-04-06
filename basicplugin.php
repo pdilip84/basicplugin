4eef5fb3cddfe6143abb8ed76bbb4a7cf6dda51b
pdilip84 <parmardilip2013@gmail.com>
2024-04-06 13:04:50 +0530

    ignore this commit

--- basicplugin.php
+++ basicplugin.php
@@ -12,6 +12,8 @@
 */
 
 /*
+You can create a basic custom plugin by boilerplate tool like https://wppb.me/
+
  Step - 1: Create plugin file, apply security rules like blank index file & ABSPATH check.
 
  Step - 2: Run activation, deactivation & uninstall hook
@@ -19,7 +21,15 @@
  Step - 3: Create a simple short-code to see if it works or not in frontend.
 
  Step - 4: Create a new table in activation hook, truncate in deactivation hook & delete table on uninstall hook
+
+ Step - 5: How to declare default attributes/parmeters to shortcode and host to assign default value to each
+
+ Step - 6: how to add JS & CSS file to your custom plugin
+
+ Step - 7: how to fetch data from tables using wp_Query class
+ 
 */
+
 // For security purpose we creted a blank index.php file and we checked if ABSPATH is defined or not. It is defined if your plugin directory is accessed by default WordPress structure. If someone tied to access plugin file directlry through URL, It is not permitted.
 
 /* step 1*/
--- uninstall.php
+++ uninstall.php
@@ -3,6 +3,7 @@ if (!defined('WP_UNINSTALL_PLUGIN')) {
     die;
 }
 
+/* step 2*/
 global $wpdb, $table_prefix;
 
 $table_name = $table_prefix . 'basic';
